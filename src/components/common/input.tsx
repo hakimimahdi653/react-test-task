6d807ea74f9b4477935be39e676c540e874b9160
hakimi <hakimimahdi653@gmail.com>
2023-09-25 08:39:00 +0330

    change function name

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -9,7 +9,7 @@ interface IProps {
 }
 
 const Input: React.FC<IProps> = ({ onChange, value, label, ...rest }) => {
-  const handle = useCallback(
+  const handleChnage = useCallback(
     ({ target }: React.ChangeEvent<HTMLInputElement>) => {
       if (onChange) {
         onChange(target.value);
@@ -26,7 +26,7 @@ const Input: React.FC<IProps> = ({ onChange, value, label, ...rest }) => {
         </label>
       </div>
       <input
-        onChange={handle}
+        onChange={handleChnage}
         value={value}
         className="block rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         {...rest}
